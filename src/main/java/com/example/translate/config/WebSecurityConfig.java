bccf40ca1713ecf3943b57d55047a3c55ec1742e
SIMJIYEON93 <930515s@gmail.com>
2024-12-28 13:17:05 +0900

    feat: security config

---
+++ src/main/java/com/example/translate/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.translate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll());
+        return http.build();
+    }
+}
