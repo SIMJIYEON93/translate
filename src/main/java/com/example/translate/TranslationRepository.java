d324fe43f946c004b33f8e7fac4c60aaf4f31edf
SIMJIYEON93 <930515s@gmail.com>
2024-12-28 13:15:23 +0900

    feat: TranslationRepository

---
+++ src/main/java/com/example/translate/TranslationRepository.java
@@ -0,0 +1,9 @@
+package com.example.translate;
+
+import com.example.translate.entity.Translation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TranslationRepository extends JpaRepository<Translation, Long> {
+}
